22dcaf1072d294c6526678eb8c8c1a653fd016e3
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-13 10:55:20 +0300

    Add generateWorker method

--- EmployeeFabric.java
+++ EmployeeFabric.java
@@ -1,7 +1,23 @@
+import java.util.Random;
+
 /**
  * Генератор работников
 */
+import java.util.Random;
 
 public class EmployeeFabric {
+
+        private static Random random = new Random();
+
+    public static Worker generateWorker(){
+        String[] names = new String[] { "Анатолий", "Глеб", "Клим", "Мартин", "Лазарь", "Владлен", "Клим", "Панкратий", "Рубен", "Герман" };
+        String[] surnames = new String[] { "Григорьев", "Фокин", "Шестаков", "Хохлов", "Шубин", "Бирюков", "Копылов", "Горбунов", "Лыткин", "Соколов" };
+        int age = random.nextInt(18, 55);
+        int salary = random.nextInt(4500, 8200);
+        return Worker.create(
+                surnames[random.nextInt(surnames.length)],
+                names[random.nextInt(names.length)],
+                age, salary);
+    }
 
 }
