d8a051a952b09cab6ce8d49ad4708c5f78159b6c
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-13 10:31:19 +0300

    Add poles

--- Employee.java
+++ Employee.java
@@ -1,6 +1,28 @@
 
 public class Employee {
 
-    
+        /**
+     * Имя
+     */
+    protected String name;
+
+    /**
+     * Фамилия
+     */
+    protected String surName;
+
+    /**
+     * Возраст
+     */
+    protected int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Ставка заработной платы
+     */
+    protected double salary;    
 
 }
