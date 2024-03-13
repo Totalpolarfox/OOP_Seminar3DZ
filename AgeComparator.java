4f506cec96c2c988413c27cf7f509673d39f3efa
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-13 11:12:28 +0300

    Redefining compare method

--- AgeComparator.java
+++ AgeComparator.java
@@ -1,9 +1,13 @@
+import java.util.Comparator;
+
 /**
  * Сортировка по возрасту
  */
 
-public class AgeComparator {
+public class AgeComparator implements Comparator<Employee> {
 
-    
+    public int compare(Employee o1, Employee o2) {
+        return Integer.compare(o2.getAge(), o1.getAge()); // сравнивает 2 значения типа Int
 
+    }
 }
