6929057daff968ad232066e978ef457baa3b0c37
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-13 10:46:45 +0300

    Add constructor

--- Freelancer.java
+++ Freelancer.java
@@ -2,6 +2,14 @@
  * Фрилансер (работник с почасовой оплатой)
 */
 
-public class Freelancer {
+public class Freelancer extends Employee {
+
+    private Freelancer(String surName, String name, int age, double salary) {
+        super(surName, name, age, salary);
+    }
+
+    public static Freelancer create(String surName, String name, int age, double salary){
+        return new Freelancer(surName, name, age, salary);
+    }
 
 }
